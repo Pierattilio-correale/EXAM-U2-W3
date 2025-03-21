eba6313b0c1a5255c840c72da38d06248b73cb35
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-03-21 16:05:12 +0100

    adds alert on form

--- js/view.js
+++ js/view.js
@@ -3,3 +3,20 @@ myform.addEventListener("submit", function (e) {
   e.preventDefault();
   myform.reset();
 });
+
+const mybuttonTriggerAlert = document.getElementById("coloralert");
+mybuttonTriggerAlert.addEventListener("click", function (e) {
+  e.preventDefault();
+  const alert = document.getElementById("success-alert");
+
+  alert.classList.remove("alertttstart");
+  alert.classList.remove("alertttgo");
+  alert.classList.add("opacity-0");
+  alert.classList.remove("opacity-0");
+  alert.classList.add("alertttstart");
+
+  setTimeout(function () {
+    alert.classList.add("alertttgo");
+  }, 3000);
+  myform.reset();
+});
